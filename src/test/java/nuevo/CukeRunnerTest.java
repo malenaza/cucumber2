b37cf078da595835ab96aec02c17b57cbdc144c7
malenaza <malena_aza@hotmail.com>
2017-01-10 13:50:47 +0100

    Update CukeRunnerTest.java

--- src/test/java/nuevo/CukeRunnerTest.java
+++ src/test/java/nuevo/CukeRunnerTest.java
@@ -1,1 +1,17 @@
+package nuevo;
+
+import org.junit.runner.RunWith;
+ 
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+ 
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "classpath:features",
+                 glue = "nuevo",
+				 plugin = { "pretty",
+						 	"html:target/cucumber-html-report" },
+				 tags = {})
+public class CukeRunnerTest {
+ 
+}
 
