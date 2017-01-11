2a3167d7e712a430ad84b7a41edc47c088fcff04
malenaza <malena_aza@hotmail.com>
2017-01-12 00:46:21 +0100

    Update Pasos.java

--- src/test/java/nuevo/Pasos.java
+++ src/test/java/nuevo/Pasos.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
+import java.util.concurrent.TimeUnit;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
