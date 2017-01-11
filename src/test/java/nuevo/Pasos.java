fec07b6d9e65bcf6921a1cd9e40e034ad0a044a0
malenaza <malena_aza@hotmail.com>
2017-01-12 00:45:19 +0100

    Update Pasos.java

--- src/test/java/nuevo/Pasos.java
+++ src/test/java/nuevo/Pasos.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
